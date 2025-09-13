b921cd32fe8808704ca92fa6c5cdca25416f95ad
Carl Paton <carl.paton@gmail.com>
2025-09-13 22:22:24 +1200

    init

---
+++ src/Resolv.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Resolv.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
